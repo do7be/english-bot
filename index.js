5d37ccf01515626a0838ff58d0d2d537b8bee30a
do7be <bomsap700@gmail.com>
2017-03-10 20:28:42 +0900

    add debug

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ var http = require('http');
 var server = http.createServer(app);
 
 app.post('/webhook', function(req, res, next) {
+    console.log('POST')
     res.status(200).end();
+    console.log(req.body)
     for (var event of req.body.events) {
+        console.log(event)
         if (event.type == 'message' && event.message.text == 'ハロー') {
             var headers = {
                 'Content-Type': 'application/json',
@@ -38,6 +41,31 @@ app.post('/webhook', function(req, res, next) {
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 server.listen(port)
+server.on('error', onError);
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
 
 function normalizePort(val) {
     var port = parseInt(val, 10);
